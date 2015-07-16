e24608d1731b9f6ea0e39754be1d0474339bdf53
Alexandr Spesivtsev <sasik@localhost.localdomain>
2015-07-16 15:42:23 +0700

    добавил интерфейс

--- src/Sasik/Db/Db.php
+++ src/Sasik/Db/Db.php
@@ -8,6 +8,69 @@
 namespace Sasik\Db;
 
 
+use Sasik\Models\Children;
+use Sasik\Models\Parents;
+use Sasik\Models\Tokens;
+
 class Db {
+
+    public $childrens = [];
+
+    public $parents = [];
+
+    public $tokens = [];
+
+    /**
+     * Db constructor.
+     * @param array $tokens
+     */
+    public function __construct()
+    {
+        $this->childrens = [
+            new Children('Dina'),
+            new Children('Alex'),
+            new Children('Ivan')
+        ];
+
+        $this->parents = [
+            new Parents($this->childrens[1]),
+            new Parents($this->childrens[1]),
+            new Parents($this->childrens[2]),
+            new Parents($this->childrens[2]),
+            new Parents($this->childrens[3]),
+        ];
+
+        $this->tokens = [
+            new Tokens($this->parents[0]),
+            new Tokens($this->parents[1]),
+            new Tokens($this->parents[2]),
+            new Tokens($this->parents[3]),
+            new Tokens($this->parents[4]),
+        ];
+
+    }
+
+    /**
+     * Если пользователь существует и пароль совпадает, вернет true
+     * иначе false
+     * @param $login
+     * @param $password
+     * @return bool
+     */
+    public function validation($login, $password)
+    {
+        foreach ($this->parents as $parent) {
+            /**
+             * @var $parent Parent
+             */
+            if ($parent->login === $login) {
+                return $parent === $password;
+            }
+        }
+
+        return false;
+
+    }
+
 
 }
---
+++ src/Sasik/Logic/AbstractLogic.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sasik
+ * Date: 7/16/15
+ * Time: 3:34 PM
+ */
+
+namespace Sasik\Logic;
+
+/**
+ *
+ * Interface AbstractLogic
+ * определения методов, которые буду дергать
+ * для реализации
+ * @package Sasik\Logic
+ */
+interface AbstractLogic
+{
+
+    /**
+     * Авторизация родителя
+     * (я присылаю с телефона номер телефона и пароль,
+     * в ответ жду 200 если все хорошо,
+     * и 401 если такого пользователя нет или лог/пасс неверные.
+     * В обоих случаях в теле ответа жду пустой json{})
+     * @param $login
+     * @param $password
+     * @return mixed
+     */
+    public function validation($login, $password);
+
+    /**
+     * Добавление токена для устройства
+     * (я присылаю логин, пароль, тип устройства, и токен)
+     * ты смотришь, если такого ключа нет, то добавляешь данные в базу
+     * @param $login
+     * @param $password
+     * @param $device
+     * @param $token
+     * @return mixed
+     */
+    public function addToken($login, $password, $device, $token);
+
+    /**
+     * Ребенок пришел/ушел в/из школу - на вход получаешь id ребенка и тип действия
+     * (0-пришел в школу, 1-ушел из школы)Находишь всех родителей ребенка,
+     * и делаешь запрос к серверу GCM как написано вот тут
+     * https://developers.google.com/cloud-messaging/
+     * @param $childId
+     * @param $eventType
+     * @return mixed
+     */
+    public function event($childId, $eventType);
+
+
+}
